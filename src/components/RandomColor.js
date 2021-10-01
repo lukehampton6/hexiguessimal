5631efe208715cce9b4d430cf8873c46dade4ee8
luke <lukehampton6@gmail.com>
2021-10-01 09:41:49 -0400

    better color generator

--- src/components/RandomColor.js
+++ src/components/RandomColor.js
@@ -1,10 +1,17 @@
 function RandomColor() {
-    const randomCode = Math.floor(Math.random()*16777215).toString(16);
-    const randomColor = "#" + randomCode;
+    // const randomCode = Math.floor(Math.random()*16777215).toString(16);
+    // const randomColor = "#" + randomCode;
+    const generateColor = function() {
+       let randomColor = '#'
+       while (randomColor.length < 7) {
+           randomColor += (Math.round(Math.random() * 15)).toString(16)
+       }
+       return randomColor;
+    }
 
     return (
-        <div>
-            <h1 style={{color: randomColor}}>{randomColor}</h1>
+        <div style={{backgroundColor: generateColor()}}>
+            <h1 style={{color: generateColor()}}>{generateColor()}</h1>
         </div>
     )
 }
